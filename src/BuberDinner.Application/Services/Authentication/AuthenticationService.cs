3c1881b992fa43e0d5dc33365bcadf2e8ed785d7
Allysson Santos <allysson.lp@gmail.com>
2023-10-16 14:34:12 -0700

    Part 2.

--- src/BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ src/BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,29 +1,39 @@
-using System;
-namespace BuberDinner.Application.Services.Authentication;
+using BuberDinner.Application.Common.Interfaces.Authentication;
 
-public interface IAuthenticationService
-{
-    AuthenticationResult Login(string email, string password);
-    AuthenticationResult Register(string firstName, string lastName, string email, string password);
-}
+namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _tokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator tokenGenerator)
+    {
+        _tokenGenerator = tokenGenerator;
+    }
+
     public AuthenticationResult Login(string email, string password)
     {
-        return new(Guid.NewGuid(),
+        var userId = Guid.NewGuid();
+        
+        var token = _tokenGenerator.GenerateToken(userId, "x", "y");
+
+        return new(userId,
             "x",
             "y",
             email,
-            "token");
+            token);
     }
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
-        return new(Guid.NewGuid(),
+        var userId = Guid.NewGuid();
+        
+        var token = _tokenGenerator.GenerateToken(userId, "x", "y");
+
+        return new(userId,
             firstName,
             lastName,
             email,
-            "token");
+            token);
     }
 }
